eadce3f502662114a4589f6f91edfc7687c7ee6a
Benard Kiplangat <benardkiplangat97@gmail.com>
2024-05-13 04:43:08 +0300

    Update 0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,7 +4,26 @@ UTF-8 validator
 """
 
 def validUTF8(data):
-    for i in data:
-        if ((not isinstance(i, int)) or i > 128 or i < 0):
-            return False
-    return True
+   num_bytes = 0
+
+    for number in data:
+        binary_result = format(number, '#010b')[-8:]
+
+        if num_bytes == 0:
+            for bit in binary_result:
+                if bit == '0':
+                    break
+                num_bytes += 1
+
+            if num_bytes == 0:
+                continue
+
+            if num_bytes == 1 or num_bytes > 4:
+                return False
+        else:
+            if not (binary_result[0] == '1' and binary_result[1] == '0'):
+                return False
+
+        num_bytes -= 1
+
+    return num_bytes == 0
