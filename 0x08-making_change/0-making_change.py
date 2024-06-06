4970536bdd985ccd4d5ff31cc59a9f7903498b71
benard Kiplangat <benardkiplangat97@gmail.com>
2024-06-06 22:08:25 +0300

    Code to calculate the number of coins

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,8 +1,29 @@
 #!/usr/bin/python3
-
+"""A script that calculates the minimal amount of change"""
 
 def makeChange(coins, total):
+    """The makeChange function to calculate the number of coins
+        Args:
+            coins: the value of coins available
+            total: the total amount
+    """
+
     if (total <= 0):
         return 0
+
+    """First sort the coins in descending order"""
+    coins.sort(reverse=True)
+
+    amountChecked = 0
+    numberCoins = 0
+
+    for i in coins:
+        while amountChecked < total:
+            amountChecked = amountChecked + i;
+            numberCoins++
+        if amountChecked == total:
+            return numberCoins
+        amountChecked--
+        numberCoins--
 
     return -1
