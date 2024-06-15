78c4592e5d1ad186e8a0a51d7367b99fc7c990d8
benard Kiplangat <benardkiplangat97@gmail.com>
2024-06-15 21:26:48 +0300

    Fixed pycodestyle errors

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -2,6 +2,8 @@
 """
 Perimeter function to calculate the perimeter of an island described in grid
 """
+
+
 def island_perimeter(grid):
     """Returns the perimeter of the island described in grid"""
     p = 0
