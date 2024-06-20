7128a2f59e1d270a6e162e743eacc95f7a858730
benard Kiplangat <benardkiplangat97@gmail.com>
2024-06-20 23:47:43 +0300

    Used Eratosthenes prime seiving algorithim to solve the prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Nunbes game
+Prime number numbers game
 """
 
 
@@ -8,7 +8,29 @@ def isWinner(x, nums):
     """
     A function to determine the winner
     """
-    ben = "Ben"
-    maria = "Maria"
+    if x < 1 or not nums:
+        return None
 
-    return None
+    marias_wins = 0
+    bens_wins = 0
+
+    n = max(nums)
+
+    primes = [True for _ in range(1, n + 1, 1)]
+    primes[0] = False
+
+    for i, is_prime in enumerate(primes, 1):
+        if i == 1 or not is_prime:
+            continue
+        for j in range(i + i, n + 1, i):
+            primes[j - 1] = False
+
+    for _, n in zip(range(x), nums):
+        primes_count = len(list(filter(lambda x: x, primes[0: n])))
+        bens_wins += primes_count % 2 == 0
+        marias_wins += primes_count % 2 == 1
+
+    if marias_wins == bens_wins:
+        return None
+
+    return 'Maria' if marias_wins > bens_wins else 'Ben'
