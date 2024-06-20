e9400749d1e5cdefd694a9bf21838e80517bd55d
benard Kiplangat <benardkiplangat97@gmail.com>
2024-06-20 12:20:53 +0300

    Initializing files

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Nunbes game
+"""
+
+
+def isWinner(x, nums):
+    """
+    A function to determine the winner
+    """
+    ben = "Ben"
+    maria = "Maria"
+
+    return None
